a2c154a2e274b08c34d41a2ea5ee3d119d709c18
Nathan Eudeline <bafbi3@gmail.com>
2026-04-22 18:34:05 +0200

    fix(repo-query): update JSON schema version to draft 2020-12

--- extensions/repo-query/scripts/generate-schema.ts
+++ extensions/repo-query/scripts/generate-schema.ts
@@ -4,7 +4,7 @@ import { join } from "node:path";
 import { RepoQueryConfigSchema } from "../src/config.js";
 
 const schema = {
-	$schema: "http://json-schema.org/draft-07/schema#",
+	$schema: "https://json-schema.org/draft/2020-12/schema#",
 	...RepoQueryConfigSchema,
 };
 
